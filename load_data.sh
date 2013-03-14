d061fbc8d41a2ee6de75e145a87876a85ef8cf37
Erling Wegger Linde <erlingwl@gmail.com>
2013-03-14 08:48:18 +0100

    removed broken link

--- load_data.sh
+++ load_data.sh
@@ -1,4 +1,4 @@
-for i in 118 123 125 126 133 141 144 148 160 161 170 177 179 194
+for i in 123 125 126 133 141 144 148 160 161 170 177 179 194
 do
 curl -s http://boosterconf.no/talks/$i.json | xargs -0 echo | curl -X POST "http://localhost:9200/talks/talk/$i" -d @-
 done
